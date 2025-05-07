16535de58e39de6fbe03cf35914020a439d79ae3
Santiago-AGZ <santiago.alvarez.gutierrez@correounivalle.edu.co>
2025-05-06 19:01:59 -0500

    feat: add price formatting and product preparation functions

---
+++ src/helpers/index.ts
@@ -0,0 +1,49 @@
+import { Color, Product, VariantProduct } from "@/interfaces/product.interface";
+
+// Función para formatear el precio a pesos colombianos
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
+};
+
+// Función para preparar los productos - (CELULARES)
+export const prepareProducts = (products: Product[]) => {
+  return products.map((product) => {
+    // Agrupar las variantes por color
+    const colors = product.variants.reduce(
+      (acc: Color[], variant: VariantProduct) => {
+        const existingColor = acc.find((item) => item.color === variant.color);
+
+        if (existingColor) {
+          // Si ya existe el color, comparamos los precios
+          existingColor.price = Math.min(existingColor.price, variant.price);
+        } // Mantenemos el precio mínimo
+        else {
+          acc.push({
+            color: variant.color,
+            price: variant.price,
+            name: variant.color_name,
+          });
+        }
+
+        return acc;
+      },
+      []
+    );
+
+    // Obtener el precio más bajo de las variantes agrupadas
+    const price = Math.min(...colors.map((item) => item.price));
+
+    // Devolver el producto formateado
+    return {
+      ...product,
+      price,
+      colors: colors.map(({ name, color }) => ({ name, color })),
+      variants: product.variants,
+    };
+  });
+};
