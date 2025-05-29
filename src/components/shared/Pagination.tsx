79f052685fbdaf191eeb402d1a6d549844122616
Santiago-AGZ <santiago.alvarez.gutierrez@correounivalle.edu.co>
2025-05-29 14:50:47 -0500

    feat: enhance product fetching and filtering functionality with pagination support

---
+++ src/components/shared/Pagination.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    totalItems: number;
+    page: number;
+}
+
+export const Pagination = () => {
+  return (
+    <div>
+      <button>Previous</button>
+      <button>Next</button>
+    </div>
+  );
+};
