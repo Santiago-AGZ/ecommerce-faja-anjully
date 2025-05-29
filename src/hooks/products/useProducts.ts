5f1cfde1813b90dbfeb7c59a4a224946c470fd26
Santiago-AGZ <santiago.alvarez.gutierrez@correounivalle.edu.co>
2025-05-29 15:10:11 -0500

    refactor: Standardize import formatting and simplify getproducts function call in useProducts hook

--- src/hooks/products/useProducts.ts
+++ src/hooks/products/useProducts.ts
@@ -1,15 +1,15 @@
-	import { getproducts } from '@/components/actions';
-import { useQuery } from '@tanstack/react-query';
+import { getproducts } from "@/components/actions";
+import { useQuery } from "@tanstack/react-query";
 
-	export const useProducts = ({ page = 1 }: { page?: number }) => {
-		const { data, isLoading } = useQuery({
-			queryKey: ['products', page],
-			queryFn: () => getproducts(page),
-			staleTime: 1000 * 60 * 5, // 1 hora
-		});
+export const useProducts = ({ page = 1 }: { page?: number }) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["products", page],
+    queryFn: () => getproducts(),
+    staleTime: 1000 * 60 * 5, // 1 hora
+  });
 
-		return {
-			products: data?.products,
-			isLoading,
-		};
-	};
+  return {
+    products: data?.products,
+    isLoading,
+  };
+};
