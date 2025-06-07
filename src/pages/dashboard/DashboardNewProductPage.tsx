d345285f2dbe485614463c1047772c9112452edf
Santiago-AGZ <santiago.alvarez.gutierrez@correounivalle.edu.co>
2025-06-07 01:42:56 -0500

    feat: update DashboardNewProductPage to include title in FormProduct component

--- src/pages/dashboard/DashboardNewProductPage.tsx
+++ src/pages/dashboard/DashboardNewProductPage.tsx
@@ -1,9 +1,9 @@
 import { FormProduct } from "@/components/dashboard";
 
 export const DashboardNewProductPage = () => {
-    return (
-        <div>
-          <FormProduct/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <FormProduct titleForm="Agregar Producto" />
+    </div>
+  );
+};
